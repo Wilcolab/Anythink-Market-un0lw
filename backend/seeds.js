9fc928d35d9a665457136d87741a1edb608279d3
Oryam <48219965+or-yam@users.noreply.github.com>
2022-05-02 12:59:48 +0300

    Create seeds script

--- backend/seeds.js
+++ backend/seeds.js
@@ -1,1 +1,43 @@
-console.log('running seeds');
+const crypto = require('crypto');
+const mongoose = require('mongoose');
+
+const NUMBER_OF_ITEMS = 42;
+
+mongoose.connect(process.env.MONGODB_URI);
+require('./models/User');
+require('./models/Item');
+
+const User = mongoose.model('User');
+const Item = mongoose.model('Item');
+
+async function createUser() {
+  const userUniqueId = crypto.randomUUID().split('-')[0];
+  const user = new User();
+  user.username = `test${userUniqueId}`;
+  user.email = `test+${userUniqueId}@test.com`;
+  user.setPassword('1234');
+  await user.save();
+  return user._id;
+}
+
+function createItem(userId) {
+  const itemUniqueId = crypto.randomUUID().split('-')[0];
+  const item = new Item();
+  item.slug = `itemSlug${itemUniqueId}`;
+  item.title = 'just tile';
+  item.description = 'some description';
+  item.image = 'https://placebear.com/200/200';
+  item.tagList = ['bear', 'test'];
+  item.seller = userId;
+  item.save();
+}
+
+async function populate() {
+  const userId = await createUser();
+  for (let i = 0; i < NUMBER_OF_ITEMS; i++) {
+    createItem(userId);
+  }
+}
+
+populate();
+
