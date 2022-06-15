54bd2378a93d0517512a32f225a8ccb91b91fa14
Oryam <48219965+or-yam@users.noreply.github.com>
2022-06-15 15:01:39 +0300

    INC-5312 |  Add default item image

--- backend/routes/api/items.js
+++ backend/routes/api/items.js
@@ -147,6 +147,10 @@ router.post("/", auth.required, function(req, res, next) {
       var item = new Item(req.body.item);
 
       item.seller = user;
+      
+      if(!item.image){
+        item.image = "/placeholder.png";
+      }
 
       return item.save().then(function() {
         sendEvent('item_created', { item: req.body.item })
