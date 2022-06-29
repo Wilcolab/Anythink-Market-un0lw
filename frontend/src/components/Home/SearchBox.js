39ff3cdffcceb381ce8bd70ba4e2425e2156389d
Oryam <48219965+or-yam@users.noreply.github.com>
2022-06-29 15:40:08 +0300

    Create search box component and update agent

---
+++ frontend/src/components/Home/SearchBox.js
@@ -0,0 +1,40 @@
+import { useState, useEffect } from 'react';
+import { SET_PAGE } from '../../constants/actionTypes';
+import { connect } from 'react-redux';
+import agent from '../../agent';
+
+const MIN_SEARCH_LENGTH = 3;
+
+const mapDispatchToProps = dispatch => ({
+  onSearch: searchInput =>
+    dispatch({
+      type: SET_PAGE,
+      tab: 'all',
+      pager: agent.Items.all,
+      payload: agent.Items.all(undefined, searchInput)
+    })
+});
+
+const SearchBox = ({ onSearch }) => {
+  const [searchInput, setSearchInput] = useState('');
+
+  useEffect(() => {
+    if (searchInput.trim().length >= MIN_SEARCH_LENGTH) {
+      onSearch(searchInput);
+    }
+  }, [searchInput, onSearch]);
+
+  return (
+    <div className="p-2 d-inline mx-2 bg-white w-100" id="search-box">
+      <input
+        className="rounded-lg border-0"
+        type="text"
+        value={searchInput}
+        onChange={e => setSearchInput(e.target.value)}
+        placeholder="What is it that you truly desire?"
+      />
+    </div>
+  );
+};
+
+export default connect(() => ({}), mapDispatchToProps)(SearchBox);
