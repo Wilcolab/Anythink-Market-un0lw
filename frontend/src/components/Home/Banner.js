ce10ce707e8ec2a1f3b1facc27e462ae0e4a2d4a
Oryam <48219965+or-yam@users.noreply.github.com>
2022-07-08 12:57:24 +0300

    Update banner

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -9,16 +9,13 @@ const Banner = () => {
       <div className="container p-4 text-center">
         <img src={logo} alt="banner" />
         <div>
-          <span>
-            A place to{" "}
-            <span
-              id="get-part"
-              onClick={() => {
-                setIsSearchBoxVisible(true);
-              }}
-            >
-              get
-            </span>
+          <span
+            id="get-part"
+            onClick={() => {
+              setIsSearchBoxVisible(true);
+            }}
+          >
+            A place to get
           </span>
           {isSearchBoxVisible && <SearchBox />}
           <span> the cool stuff.</span>
