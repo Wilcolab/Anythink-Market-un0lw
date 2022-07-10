8b382c4097f6cd128ddc1c312d451a9f3e83bba3
Oryam <48219965+or-yam@users.noreply.github.com>
2022-07-10 13:25:27 +0300

    Testing error

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -1,23 +1,15 @@
-import React, { useState } from "react";
+import React from "react";
 import logo from "../../imgs/logo.png";
 import SearchBox from "./SearchBox";
 
 const Banner = () => {
-  const [isSearchBoxVisible, setIsSearchBoxVisible] = useState(false);
-
-  const showSearchBox = () => {
-    setIsSearchBoxVisible(true);
-  };
-
   return (
     <div className="banner text-white">
       <div className="container p-4 text-center">
         <img src={logo} alt="banner" />
         <div>
-          <span id="get-part" onClick={showSearchBox}>
-            A place to get
-          </span>
-          {isSearchBoxVisible && <SearchBox />}
+          <span id="get-part">A place to get</span>
+          <SearchBox />
           <span> the cool stuff.</span>
         </div>
       </div>
