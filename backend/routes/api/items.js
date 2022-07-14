5dc8342c6354cb64aa7528d79c2f17ed41a36ba4
Oryam <48219965+or-yam@users.noreply.github.com>
2022-07-14 17:45:47 +0300

    Refactor items backend route

--- backend/routes/api/items.js
+++ backend/routes/api/items.js
@@ -36,7 +36,7 @@ router.param("comment", function(req, res, next, id) {
     .catch(next);
 });
 
-router.get("/", auth.optional, function(req, res, next) {
+router.get("/", auth.optional, async function (req, res, next) {
   var query = {};
   var limit = 100;
   var offset = 0;
@@ -53,46 +53,46 @@ router.get("/", auth.optional, function(req, res, next) {
     query.tagList = { $in: [req.query.tag] };
   }
 
-  Promise.all([
+  const [seller, favoriter] = await Promise.all([
     req.query.seller ? User.findOne({ username: req.query.seller }) : null,
-    req.query.favorited ? User.findOne({ username: req.query.favorited }) : null
-  ])
-    .then(function(results) {
-      var seller = results[0];
-      var favoriter = results[1];
+    req.query.favorited
+      ? User.findOne({ username: req.query.favorited })
+      : null,
+  ]);
 
-      if (seller) {
-        query.seller = seller._id;
-      }
+  if (seller) {
+    query.seller = seller._id;
+  }
 
-      if (favoriter) {
-        query._id = { $in: favoriter.favorites };
-      } else if (req.query.favorited) {
-        query._id = { $in: [] };
-      }
+  if (favoriter) {
+    query._id = { $in: favoriter.favorites };
+  } else if (req.query.favorited) {
+    query._id = { $in: [] };
+  }
 
-      return Promise.all([
-        Item.find(query)
-          .limit(Number(limit))
-          .skip(Number(offset))
-          .sort({ createdAt: "desc" })
-          .exec(),
-        Item.count(query).exec(),
-        req.payload ? User.findById(req.payload.id) : null
-      ]).then(async function(results) {
-        var items = results[0];
-        var itemsCount = results[1];
-        var user = results[2];
-        return res.json({
-          items: await Promise.all(
-            items.map(async function(item) {
-              item.seller = await User.findById(item.seller);
-              return item.toJSONFor(user);
-            })
-          ),
-          itemsCount: itemsCount
-        });
-      });
+  const [items, itemsCount, user] = await Promise.all([
+    Item.aggregate([
+      { $match: query },
+      { $limit: Number(limit) },
+      { $skip: Number(offset) },
+      { $sort: { createdAt: -1 } },
+      {
+        $lookup: {
+          from: "users",
+          localField: "seller",
+          foreignField: "_id",
+          as: "seller",
+        },
+      },
+    ]).exec(),
+    Item.count(query).exec(),
+    req.payload ? User.findById(req.payload.id) : null,
+  ]);
+
+  return res
+    .json({
+      items,
+      itemsCount,
     })
     .catch(next);
 });
