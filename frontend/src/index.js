860441c0e2bdabf8a94de003b68045fe1282b261
Oryam Nehoray <48219965+or-yam@users.noreply.github.com>
2023-01-22 20:46:17 +0200

    Using global styles

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,6 +5,7 @@ import React from "react";
 import { store, history } from "./store";
 import { ThemeProvider } from "styled-components";
 import theme from "./styles/theme";
+import GlobalStyle from "./styles/GlobalStyle";
 
 import { Route, Switch } from "react-router-dom";
 import { ConnectedRouter } from "react-router-redux";
@@ -13,6 +14,7 @@ import App from "./components/App";
 
 ReactDOM.render(
   <ThemeProvider theme={theme}>
+    <GlobalStyle />
     <Provider store={store}>
       <ConnectedRouter history={history}>
         <Switch>
