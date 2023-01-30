13f9d486310b5150dc08c0eac2a5e8ce0cfc01c4
WilcoApp <hello@wilco.gg>
2023-01-30 06:16:33 +0000

    CircleCI Lint

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,9 +3,6 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import React from "react";
 import { store, history } from "./store";
-import { ThemeProvider } from "styled-components";
-import theme from "./styles/theme";
-import GlobalStyle from "./styles/GlobalStyle";
 
 import { Route, Switch } from "react-router-dom";
 import { ConnectedRouter } from "react-router-redux";
@@ -13,15 +10,13 @@ import { ConnectedRouter } from "react-router-redux";
 import App from "./components/App";
 
 ReactDOM.render(
-  <ThemeProvider theme={theme}>
-    <GlobalStyle />
-    <Provider store={store}>
-      <ConnectedRouter history={history}>
-        <Switch>
-          <Route path="/" component={App} />
-        </Switch>
-      </ConnectedRouter>
-    </Provider>
-  </ThemeProvider>,
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <Switch>
+        <Route path="/" component={App} />
+      </Switch>
+    </ConnectedRouter>
+  </Provider>,
+
   document.getElementById("root")
 );
